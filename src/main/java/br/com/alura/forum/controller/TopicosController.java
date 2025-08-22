0b813160662a4871d8a9f2defa37f043dbecd286
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-08-22 16:56:43 -0300

    restController problem. Resolved!

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,9 +3,8 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.modelo.Curso;
