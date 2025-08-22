9545c8c5f0356eb48e311c552be8185c8fad1ca8
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-08-22 16:29:05 -0300

    teste

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.modelo.Curso;
@@ -13,6 +14,7 @@ import br.com.alura.forum.modelo.Curso;
 public class TopicosController {
 
 	@RequestMapping("/topicos")
+	@ResponseBody
 	public List<Topico> lista() {
 		
 		Topico topico = new Topico ("Duvida", "Duvida com spring", new Curso("Spring", "Programação"));
