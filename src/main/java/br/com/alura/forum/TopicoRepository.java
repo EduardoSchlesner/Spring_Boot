255e3f903014cad013d80d57558b7252fba5e70b
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-08-27 12:00:27 -0300

    separando curso e nome

--- src/main/java/br/com/alura/forum/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/TopicoRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-    List<Topico> findByCursoNome(String nomeCurso);
+    List<Topico> findByCurso_Nome(String nomeCurso);
 
 }
