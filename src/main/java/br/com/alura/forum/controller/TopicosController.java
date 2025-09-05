8bee2a6d15ca5c7d33150e94881f5eff21308615
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-09-05 13:44:00 -0300

    Removendo do banco de dados.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -59,8 +59,12 @@ public class TopicosController {
     @Transactional
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         Topico topico = form.atualizar(id, topicoRepository);
-
         return ResponseEntity.ok(new TopicoDto(topico));
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remover(@PathVariable Long id){
+        topicoRepository.deleteById(id);
+        return  ResponseEntity.ok().build();
+    }
 }
