2c8d5ec73435233f4898f83ce52d1362d1ca9549
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-09-08 13:42:04 -0300

    Adicionando security para o GET.

--- src/main/java/br/com/alura/forum/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +12,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    //configurações de autenticação!!!
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    //Configurações de Autorização!!!
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/").permitAll()
+                .antMatchers(HttpMethod.GET,"/topicos/*").permitAll(); //liberando acesso aos endpoints publicos
+
+
+    }
+
+    //Configuracoes de recursos estáticos (requisições de javascript, css, imagens, etc...)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
 }
