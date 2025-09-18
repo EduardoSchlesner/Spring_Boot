bae16bc20abacf07c50cd0353cea07354cba606f
Eduardo Schlesner <eduardoschlesner73@gmail.com>
2025-09-18 16:50:13 -0300

    Validando o token com as autorizações.

--- src/main/java/br/com/alura/forum/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum;
 
+import br.com.alura.forum.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +25,9 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     @Bean
     protected AuthenticationManager authenticationManager() throws Exception {
@@ -46,7 +50,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //avisando pro security para não criar sessao, utilizando token (maneira stateless).
-                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService), UsernamePasswordAuthenticationFilter.class)
+                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class)
                 ;
     }
 
